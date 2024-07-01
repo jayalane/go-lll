2f3552fc55a47c1dcb0aa0101577e86dcabe0f03
Chris Lane <chris@disputingtaste.com>
2024-06-30 20:26:21 -0700

    Makefile and linting

--- ll.go
+++ ll.go
@@ -4,7 +4,6 @@ package lll
 
 import (
 	"fmt"
-	"github.com/lestrrat-go/file-rotatelogs"
 	"io"
 	"log"
 	"math"
@@ -13,6 +12,8 @@ import (
 	"os/user"
 	"strings"
 	"sync/atomic"
+
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 )
 
 const (
@@ -22,7 +23,14 @@ const (
 	none
 )
 
-// Lll is a low level logger
+const (
+	eightMB = 8 * 1024 * 1024
+	// MaxModLen is the maximum length for the module name.
+	MaxModLen = 50
+	two       = 2
+)
+
+// Lll is a low level logger.
 type Lll struct {
 	module string
 	n      uint64 // should be a map but whatevs
@@ -33,119 +41,139 @@ type Lll struct {
 // Init is called once if you want a non-default
 // file name
 
-var initOnceDone int64
-var theWriter io.Writer
-var theLogPath string = "."
+var (
+	initOnceDone int64
+	theWriter    io.Writer
+	theLogPath   = "."
+)
 
-// initOnce nees to be called to get log rotation going
+// initOnce nees to be called to get log rotation going.
 func initOnce() {
 	swapped := atomic.CompareAndSwapInt64(&initOnceDone, 0, 1)
 	if !swapped {
 		return
 	}
+
 	// have this run twice
 	binaryFilename, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
+
 	p := strings.Split(binaryFilename, "/")
 	item := p[len(p)-1]
 	logPathTemplate := "/var/log/" + item + ".log.%Y%m%d"
+
 	u, err := user.Current()
 	if err != nil {
 		log.Panic("Can't check user id")
 	}
+
 	if u.Uid != "0" {
 		logPathTemplate = theLogPath + "/" + item + ".log.%Y%m%d"
 	}
+
 	if theWriter == nil {
 		// init rotating logs
 		t, err := rotatelogs.New(
 			logPathTemplate,
-			rotatelogs.WithRotationSize(8*1024*1024),
+			rotatelogs.WithRotationSize(eightMB),
 		)
 		if err != nil {
 			log.Panic("Can't open rotating logs")
 		}
+
 		theWriter = t
 	}
+
 	log.SetOutput(theWriter)
 }
 
-// SetLogPath sets the path for the template for non-root use.  It must be called before any Init()
+// SetLogPath sets the path for the template for non-root use.  It must be called before any Init().
 func SetLogPath(logPath string) {
 	if atomic.LoadInt64(&initOnceDone) == 1 {
 		panic("SetLogPath called after Init!")
 	}
+
 	theLogPath = logPath
 }
 
 // SetWriter takes a writer and sets it up so the logger when inited
-// will use this writer level
+// will use this writer level.
 func SetWriter(writer io.Writer) {
 	theWriter = writer
 }
 
-// Init takes a module name and a level string and returns a logger
+// Init takes a module name and a level string and returns a logger.
 func Init(modName string, level string) *Lll {
 	initOnce()
-	if len(modName) > 50 {
+
+	if len(modName) > MaxModLen {
 		log.Panic("Init lll called with giant module name", modName)
 	}
+
 	l := log.New(theWriter, modName, 0)
+
 	l.SetPrefix(modName + " ")
 	l.SetOutput(theWriter)
 	l.SetFlags(log.Ldate + log.Ltime + log.Lmsgprefix + log.Lmicroseconds)
+
 	res := Lll{module: modName, log: l, level: all}
 	res.SetLevel(level)
+
 	return &res
 }
 
-// SetLevel global needed for backward compatibility
+// SetLevel global needed for backward compatibility.
 func SetLevel(l *Lll, level string) {
 	l.SetLevel(level)
 }
 
-// GetLevel needed for go-globals tests
+// GetLevel needed for go-globals tests.
 func GetLevel(l *Lll) int {
 	return int(atomic.LoadInt64(&l.level))
 }
 
-// GetLevel returns the level for this specific logger instance
+// GetLevel returns the level for this specific logger instance.
 func (ll *Lll) GetLevel() int {
 	return int(atomic.LoadInt64(&ll.level))
 }
 
 // SetLevel takes a low level logger and a level string and resets the
-// log level
+// log level.
 func (ll *Lll) SetLevel(level string) {
 	var theLev int64
-	if level == "network" {
+
+	switch level {
+	case "network":
 		theLev = network
-	} else if level == "none" {
+	case "none":
 		theLev = none
-	} else if level == "state" {
+	case "state":
 		theLev = state
-	} else {
+	default:
 		theLev = all
 	}
+
 	atomic.StoreInt64(&ll.level, theLev)
 	fmt.Println("Set level to", atomic.LoadInt64(&ll.level), theLev)
 }
 
-// Ln is Log Network - most volumunous
+// Ln is Log Network - most volumunous.
 func (ll *Lll) Ln(ls ...interface{}) {
 	if atomic.LoadInt64(&ll.level) > network {
 		return
 	}
+
 	ll.log.Println(ls...)
 }
 
-// Ls is Log State - TCP reads/writes (but not what), accept/close
+// Ls is Log State - TCP reads/writes (but not what), accept/close.
 func (ll *Lll) Ls(ls ...interface{}) {
 	if atomic.LoadInt64(&ll.level) > state {
 		return
 	}
+
 	ll.log.Println(ls...)
 }
 
@@ -154,18 +182,20 @@ func (ll *Lll) La(ls ...interface{}) {
 	if atomic.LoadInt64(&ll.level) > all {
 		return
 	}
+
 	ll.log.Println(ls...)
 }
 
-// Ll is Log Lots ays - starts out logging all then 1/N then every 60,000th
+// Ll is Log Lots ays - starts out logging all then 1/N then every 60,000th.
 func (ll *Lll) Ll(ls ...interface{}) {
 	if atomic.LoadInt64(&ll.level) > all {
 		return
 	}
+
 	numLoggeds := atomic.AddUint64(&ll.n, 1)
 
 	if numLoggeds%50000 == 0 ||
-		rand.Float64() < 1.0/float64(math.Pow(math.Log(1+float64(numLoggeds)), 2)) {
+		rand.Float64() < 1.0/float64(math.Pow(math.Log(1+float64(numLoggeds)), two)) { //nolint:gosec
 		ll.log.Println(ls...)
 	}
 }
--- ll_test.go
+++ ll_test.go
@@ -7,139 +7,178 @@ import (
 	"bytes"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"sync"
 	"testing"
 	"time"
 )
 
-// Buffer type from stackoverflow for safe concurrency
+// Buffer type from stackoverflow for safe concurrency.
 type Buffer struct {
 	b bytes.Buffer
 	m sync.Mutex
 }
 
-func (b *Buffer) Read(p []byte) (n int, err error) {
+func (b *Buffer) Read(p []byte) (int, error) {
 	b.m.Lock()
 	defer b.m.Unlock()
+
 	return b.b.Read(p)
 }
-func (b *Buffer) Write(p []byte) (n int, err error) {
+
+func (b *Buffer) Write(p []byte) (int, error) {
 	b.m.Lock()
 	defer b.m.Unlock()
+
 	return b.b.Write(p)
 }
+
 func (b *Buffer) String() string {
 	b.m.Lock()
 	defer b.m.Unlock()
+
 	return b.b.String()
 }
 
-func TestNormal(t *testing.T) {
+func TestNormal(_ *testing.T) {
 	var ml *Lll
+
 	SetLogPath("/tmp")
+
 	ml = Init("TEST", "network")
+
 	ml.La("hi there see if it works")
 }
 
 func TestLa(t *testing.T) {
-
 	var ml *Lll
-	var buffer = new(Buffer)
-	var modName = "TEST"
-	var msgString = "hi"
-	var numLogs = 1000
+
+	buffer := new(Buffer)
+	modName := "TEST"
+	msgString := "hi"
+	numLogs := 1000
 
 	SetWriter(buffer)
+
 	ml = Init("TEST", "debug")
-	for i := 0; i < numLogs; i++ {
-		ml.La("hi" + fmt.Sprint(i))
+
+	for i := range numLogs {
+		ml.La("hi" + strconv.Itoa(i))
 	}
+
 	time.Sleep(1100 * time.Millisecond)
+
 	scanner := bufio.NewScanner(buffer)
 	i := 0
+
 	for scanner.Scan() {
 		l := scanner.Text()
 		sections := strings.Split(l, " ")
+
 		if len(sections) != 4 {
 			t.Fatal("Wrong format", l, len(sections))
+
 			return
 		}
+
 		if modName != sections[2] {
 			t.Fatal("Wrong module", l, sections[2])
+
 			return
 		}
-		if !(msgString+fmt.Sprint(i) == sections[3]) {
+
+		if !(msgString+strconv.Itoa(i) == sections[3]) {
 			t.Fatal("Wrong log msg", l, sections[3])
+
 			return
 		}
+
 		i++
 	}
 
 	if err := scanner.Err(); err != nil {
 		t.Fatal(err)
+
 		return
 	}
+
 	if i != numLogs {
 		t.Fatal("Not enough logs got", i, "wanted", numLogs)
 	}
-	// all good
 }
 
 func TestLl(t *testing.T) {
-
 	var ml *Lll
-	var buffer = new(Buffer)
-	var modName = "TEST"
-	var msgString = "yo_this_is_a_longer_string_maybe_some_memory_thing_z"
-	var numLogs = 10000
+
+	buffer := new(Buffer)
+	modName := "TEST"
+	msgString := "yo_this_is_a_longer_string_maybe_some_memory_thing_z"
+	numLogs := 10000
+
 	SetWriter(buffer)
+
 	ml = Init("TEST", "debug")
+
 	ml.SetLevel("network")
-	for i := 0; i < numLogs; i++ {
+
+	for i := range numLogs {
 		go func(j int) {
-			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-			ml.Ll(msgString + fmt.Sprint(j))
-			ml.Ln(msgString + fmt.Sprint(j) + "whatevs")
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond) //nolint:gosec
+			ml.Ll(msgString + strconv.Itoa(j))
+			ml.Ln(msgString + strconv.Itoa(j) + "whatevs")
 		}(i)
 	}
+
 	ml.SetLevel("all")
-	for i := 0; i < numLogs; i++ {
+
+	for i := range numLogs {
 		go func(j int) {
-			ml.Ll(msgString + fmt.Sprint(j) + "2")
-			ml.Ln(msgString + fmt.Sprint(j) + "whatevs2")
+			ml.Ll(msgString + strconv.Itoa(j) + "2")
+			ml.Ln(msgString + strconv.Itoa(j) + "whatevs2")
 		}(i)
 	}
+
 	time.Sleep(1100 * time.Millisecond)
+
 	scanner := bufio.NewScanner(buffer)
 	i := 0
+
 	for scanner.Scan() {
 		l := scanner.Text()
 		fmt.Println("Got a line:", l)
 		sections := strings.Split(l, " ")
+
 		if len(sections) != 4 {
 			t.Fatal("Wrong format", l, len(sections))
+
 			return
 		}
+
 		if modName != sections[2] {
 			t.Fatal("Wrong module", l, sections[2])
+
 			return
 		}
+
 		if !(msgString == sections[3][0:len(msgString)]) {
 			t.Fatal("Wrong log msg", l, sections[3])
+
 			return
 		}
+
 		i++
 	}
 
 	if err := scanner.Err(); err != nil {
 		t.Fatal(err)
+
 		return
 	}
 
 	if i > numLogs/2 {
 		t.Fatal("Too many logs got", i, "wanted a small fraction of", numLogs)
 	}
+
 	t.Log("Got", i)
-	// all good
 }
